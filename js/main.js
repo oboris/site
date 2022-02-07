3e6aa114539284010ea0d323b0972ce025abb02a
Boris Onyshchenko <oboris@ukr.net>
2022-02-07 14:29:18 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,85 @@
+const area = document.getElementById('area');
+let isCrossOrder = true;
+let dim = 3;
+var field = [];
+
+area.addEventListener('click', e => {
+    if(e.target.className == 'box' && e.target.innerHTML == ''){
+        if (isCrossOrder) {
+            e.target.innerHTML = 'X';
+        } else {
+            e.target.innerHTML = '0';
+        }
+        isCrossOrder = !isCrossOrder;
+        checkWinner();
+    }
+})
+
+function checkWinner() {
+    const boxes = document.getElementsByClassName('box');
+    field = new Array(dim);
+    let ind = 0;
+    let winner = "";
+    for (let i = 0; i < dim; i++) {
+        field[i] = new Array(dim);
+        for (let j = 0; j < dim; j++){
+            field[i][j] = boxes[ind].innerHTML;
+            ind++;
+        }
+    }
+    if (checkVertical('X') || checkHorisontal('X') || checkDiahonal('X')){
+        winner = 'X';
+    } else  if (checkVertical('0') || checkHorisontal('0') || checkDiahonal('0')){
+        winner = '0';
+    }
+    if (winner != ''){
+        document.getElementById('tit').innerHTML = winner;
+    }
+}
+
+function checkVertical(sign){
+    for (let i = 0; i < dim; i++) {
+        let count = 0;
+        for (let j = 0; j < dim; j++){
+            if (field[i][j] == sign){
+                count++;
+            }
+        }
+        if (count == dim){
+            return true;
+        }
+    } 
+    return false;
+}
+
+function checkHorisontal(sign) {
+    for (let j = 0; j < dim; j++) {
+        let count = 0;
+        for (let i = 0; i < dim; i++){
+            if (field[i][j] == sign){
+                count++;
+            }
+        }
+        if (count == dim){
+            return true;
+        }
+    } 
+    return false;
+}
+
+function checkDiahonal(sign) {
+    let countMain = 0;
+    let countOther = 0;
+    for (let i = 0; i < dim; i++) {
+        if (field[i][i] == sign) {
+            countMain++;
+        }
+        if (field[i][dim - i - 1] == sign) {
+            countOther++;
+        }
+    }
+    if (countMain == dim || countOther == dim) {
+        return true;
+    }
+    return false;
+}
